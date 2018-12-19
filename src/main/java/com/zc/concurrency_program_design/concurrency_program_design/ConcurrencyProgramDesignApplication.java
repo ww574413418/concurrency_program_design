78785ac1eac1591b04a7dbcc0a279b1c27a9efb9
张超 <574413418@qq.com>
2018-12-19 21:21:56 +0800

    Initial commit

---
+++ src/main/java/com/zc/concurrency_program_design/concurrency_program_design/ConcurrencyProgramDesignApplication.java
@@ -0,0 +1,14 @@
+package com.zc.concurrency_program_design.concurrency_program_design;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurrencyProgramDesignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcurrencyProgramDesignApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/zc/concurrency_program_design/concurrency_program_design/ConcurrencyProgramDesignApplicationTests.java
@@ -0,0 +1,17 @@
+package com.zc.concurrency_program_design.concurrency_program_design;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ConcurrencyProgramDesignApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
